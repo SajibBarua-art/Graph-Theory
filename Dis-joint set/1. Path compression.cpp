ef795f1738c1cfcb7503ed62d32d649ce94530f2
SajibBarua-art <sajibbaruanstu@gmail.com>
2021-10-14 11:52:42 +0600

    Day 8

---
+++ Dis-joint set/1. Path compression.cpp
@@ -0,0 +1,46 @@
+// Disjoint set
+// Problem Link: https://assessment.hackerearth.com/challenges/college/codemania-20/algorithm/owl-fight/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int ar[10004];
+
+int findParent(int node) {
+	if(ar[node]<0) return node;
+
+	return ar[node] = findParent(ar[node]);
+}
+
+void Union(int a, int b) {
+	ar[a] = min(ar[a], ar[b]);
+	ar[b] = a;
+}
+
+int main() {
+	int n, m; cin >> n >> m;
+	
+	for(int i=1; i<=n; i++) ar[i] = -i;
+
+	for(int i=1; i<=m; i++) {
+		int a, b; cin >> a >> b;
+
+		a = findParent(a);
+		b = findParent(b);
+
+		if(a != b) Union(a, b);
+	}
+
+	int q; cin >> q;
+	for(int i=1; i<=q; i++) {
+		int a, b; cin >> a >> b;
+
+		int x = findParent(a); int y = findParent(b);
+
+		if(x == y) cout << "TIE\n";
+		else{
+			if(ar[x] < ar[y]) cout << a << endl;
+			else cout << b << endl;
+		}
+	}
+}
